5a765438d9ea7286122b2be4fad8cb7ca99dc13f
代码之力 <9770+ShiningRay@users.noreply.github.com>
2023-01-31 23:46:23 +0800

    Update dependencies.sh
    ignore platform when prune

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -179,7 +179,7 @@ yarn_prune_devdependencies() {
     meta_set "skipped-prune" "false"
   else
     cd "$build_dir" || return
-    monitor "yarn-prune" yarn install --frozen-lockfile --ignore-engines --ignore-scripts --prefer-offline 2>&1
+    monitor "yarn-prune" yarn install --frozen-lockfile --ignore-engines --ignore-platform --ignore-scripts --prefer-offline 2>&1
     meta_set "skipped-prune" "false"
   fi
 }
